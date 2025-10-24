8b1fbb06d1304643e2c41b9869f81741169ae2ec
Kaunfi <166756176+Kaunfi@users.noreply.github.com>
2025-10-24 23:47:46 +0200

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,27 +3,31 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-  // TODO: Add your own account association
-    accountAssociation": {
-    "header": "eyJmaWQiOjgwMDQ1NywidHlwZSI6ImF1dGgiLCJrZXkiOiIweDQxYjgzNEE0Q0MwMEE3NUUyQkM3MkYyMWU3YWMyQTc1NTQzNThDYmEifQ",
-    "payload": "eyJkb21haW4iOiJmYXJjYXN0ZXItbWluaWFwcC1vbmUudmVyY2VsLmFwcCJ9",
-    "signature": "bgBepVbBUT4h0vz0FdYJekeddLDxVxZsIs/it43m4WN3klQeqf0Yw/q4+FSfy1BV2EaP4wPQC/X6SRYaHS4osBs="
+  "accountAssociation": {
+    "header": "eyJmaWQiOjMyODg1NSwidHlwZSI6ImF1dGgiLCJrZXkiOiIweERCOTczZjliNWI0MDhFMkZFNjNFQ2UzNjBEYjFjODU5YjQ5NDFFRTcifQ",
+    "payload": "eyJkb21haW4iOiJmYXJjYXN0ZXItbWluaWFwcC10ZW1wbGF0ZS1lYm9uLnZlcmNlbC5hcHAifQ",
+    "signature": "1bH6yx1yjJgF2olNGcmhAgsbS229TmERlvHsktnOqA88aDMQAPm+WSFEgqfIFtcRFIUsewD5Of72EVNKufxX3Bs="
   },
-    frame: {
-      version: "1",
-      name: "Trankil Farcaster MiniApp",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ["Base", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-    },
-  };
+  "frame": {
+    "version": "1",
+    "name": "Trankil Farcaster MiniApp",
+    "iconUrl": "https://farcaster-miniapp-one.vercel.app//images/icon.png",
+    "homeUrl": "https://farcaster-miniapp-one.vercel.app/",
+    "imageUrl": "https://farcaster-miniapp-one.vercel.app//images/feed.png",
+    "screenshotUrls": [],
+    "tags": [
+      "Base",
+      "farcaster",
+      "miniapp",
+      "template"
+    ],
+    "primaryCategory": "developer-tools",
+    "buttonTitle": "Launch Template",
+    "splashImageUrl": "https://farcaster-miniapp-one.vercel.app//images/splash.png",
+    "splashBackgroundColor": "#ffffff",
+    "webhookUrl": "https://farcaster-miniapp-one.vercel.app//api/webhook"
+  }
+};
 
   return NextResponse.json(farcasterConfig);
 }
